8bbe23c14d3a4ad603d4949522d935b1b749c4a7
Tyler <t19.olson@gmail.com>
2022-10-18 16:30:28 -0400

    delete working

--- server.js
+++ server.js
@@ -2,31 +2,29 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const uuid = require('./helpers/uuid.js');
-const noteData = require('./db/db.json');
-
-
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
+let noteData = path.join(__dirname, './db/db.json');
+let notes = JSON.parse(fs.readFileSync(noteData)) || [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
 
 
-
-
+// Route for index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 })
-
+//ROute for notes.html
 app.get('/notes', (req, res)  => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 })
-
+// Route for the db.json file
 app.get('/api/notes', (req, res) => {
-  return res.json(noteData)
+  return res.json(notes)
   
 })
 
@@ -37,28 +35,24 @@ app.post('/api/notes', (req, res) => {
       title,
       text,
       id: uuid(),
-    }
-  
-    // const noteString = JSON.stringify(newNote)
+    }    
     
-  //   fs.writeFile(`./db/db.json`, noteString, (err) =>
-  //     err
-  //       ? console.error(err)
-  //       :console.log('error')
-  //       )
-
-  
-  
-  noteData.push(newNote);
-
-  res.json(noteData)
+    notes.push(newNote);
+    
+    fs.writeFileSync(noteData, JSON.stringify(notes));
+    res.end();
 })
 
 
 
-app.delete('./api/notes/:id', (req, res) => {
-  const remove = req.params.id
-  res.json(notes)
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id
+  const index = notes.findIndex((note) => note.id === id)
+
+  notes.splice(index, 1)
+  fs.writeFileSync(noteData, JSON.stringify(notes)) 
+
+  res.end()
   }
 )
 
